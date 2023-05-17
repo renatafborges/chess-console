956c7c9bb19ea964098fa512053d7eb9a67d9962
Renata Borges <renata.borges@db1.com.br>
2023-05-17 17:10:20 -0300

    Creating class Position

--- chess-console/Program.cs
+++ chess-console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using board;
 
 namespace chess_console
 {
@@ -6,7 +7,12 @@ namespace chess_console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Position P;
+            
+            P = new Position(3, 4);
+            Console.WriteLine("Position:   " + P);
+            
+            Console.ReadLine();
         }
     }
 }
